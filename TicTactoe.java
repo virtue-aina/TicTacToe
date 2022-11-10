e02ff4d4093ad9aa2ffa99c10d8018a732558703
virtue-aina <115047899+virtue-aina@users.noreply.github.com>
2022-11-10 00:09:55 +0000

    Initial commit

---
+++ TicTactoe.java
@@ -0,0 +1,18 @@
+
+public class TicTactoe
+{
+    public static void main (String [] args)
+    {
+    //create 2d array
+        char[] [] gameBoard = { {' ', '|', ' ', ' |', ' '},
+                                {'_', '+', '_', ' +', '_'},
+                                {' ', '|', ' ', ' |', ' '},
+                                {'_', '+', '_', ' +', '_'},
+                                {' ', '|', ' ', ' |', ' '}};
+        for (char [] : gameBoard)
+             for (char c: row)
+             {System.out.print(c);}
+        System.out.println();}
+
+    }
+}
